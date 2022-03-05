df74414f7111723b9c1ea878805588cfd9adf3bc
omarserrar <omarserrar2@gmail.com>
2022-03-05 11:52:09 +0100

    Init Project

---
+++ src/main/java/com/omarserrar/textme/TextMeApplication.java
@@ -0,0 +1,13 @@
+package com.omarserrar.textme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omarserrar/textme/TextMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omarserrar.textme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
